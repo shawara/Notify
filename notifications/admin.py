61d9909503b629ac1d25a5da38f36fd1efbaa14c
shawara <m.shawara@outlook.com>
2021-06-06 23:25:54 +0200

    register model in admin panel

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Customer, Notification, Device, Group, Message
+
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
