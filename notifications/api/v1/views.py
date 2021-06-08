3fc4c5a99dfa31bde48e0e2c028e64293f5cef13
shawara <m.shawara@outlook.com>
2021-06-09 00:34:01 +0200

    add default order by to fix pagination

--- notifications/api/v1/views.py
+++ notifications/api/v1/views.py
@@ -7,24 +7,24 @@ from ...models import Message, Notification, Customer, Group, Device
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    queryset = Message.objects.all()
+    queryset = Message.objects.order_by('-pk')
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
     serializer_class = NotificationSerializer
-    queryset = Notification.objects.all()
+    queryset = Notification.objects.order_by('-pk')
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
-    queryset = Customer.objects.all()
+    queryset = Customer.objects.order_by('-pk')
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
-    queryset = Group.objects.all()
+    queryset = Group.objects.order_by('-pk')
 
 
 class DeviceViewSet(viewsets.ModelViewSet):
     serializer_class = DeviceSerializer
-    queryset = Device.objects.all()
+    queryset = Device.objects.order_by('-pk')
